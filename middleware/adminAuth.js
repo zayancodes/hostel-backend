bf62f7b6003b28a27dab1469a47da40f7d65f9d8
zayancodes <razamaani11@gmail.com>
2024-07-04 10:08:45 +0500

    backend update

---
+++ middleware/adminAuth.js
@@ -0,0 +1,13 @@
+// middleware/adminAuth.js
+module.exports = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({ 
+        message: 'Access denied: Admins only',
+        redirectTo: '/'  // Provide the URL to redirect to
+      });
+      // res.status(403).json({ message: 'Access denied: Admins only' });
+    }
+  };
+  
